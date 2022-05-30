46476bf4ba293ff444aa180d57be1a98f25965f1
salimep <salimep@gmail.com>
2022-05-30 14:05:18 +0300

    new file:   config-metallb.yml 	new file:   deploy.yml 	new file:   pod.yml 	new file:   sample-web.yml 	new file:   servic-cluster.yml 	new file:   web-2/index.html

---
+++ a.sh
@@ -0,0 +1,7 @@
+echo "# kubernetes" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/salimep/kubernetes.git
+git push -u origin main
